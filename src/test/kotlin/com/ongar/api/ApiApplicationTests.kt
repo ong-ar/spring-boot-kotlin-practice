084e401477cdf1ad61517e42c388f6829e142e3d
ong-ar <reclusis@gmail.com>
2020-09-21 11:38:27 +0900

    initial

---
+++ src/test/kotlin/com/ongar/api/ApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.ongar.api
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
